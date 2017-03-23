07aed8f6efac38882518eef82b3570e17920fb26
Julián Gutiérrez <juliangut@gmail.com>
2017-03-24 00:24:03 +0100

    fix namespace

--- tests/ORM/RelationalRepositoryFactoryTest.php
+++ tests/ORM/RelationalRepositoryFactoryTest.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Jgut\Doctrine\Repository\ORM\Tests\Factory;
+namespace Jgut\Doctrine\Repository\ORM\Tests;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
